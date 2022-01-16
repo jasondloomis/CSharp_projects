75c014be18505dbb9226e7d8f9106bd4904fc1e2
Jason Loomis <jasondloomis@icloud.com>
2022-01-15 20:35:51 -0800

    Add project files.

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Acme Accounting Systems");
+            Console.WriteLine("We are \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
