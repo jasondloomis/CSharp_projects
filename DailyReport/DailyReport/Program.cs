3e98c418ebe436c8b216e0ed3548c4a845d3c9a9
Jason Loomis <jasondloomis@icloud.com>
2022-01-17 19:29:24 -0800

    add daily report project

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Splash intro
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            // Ask user name and store in variable
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            // Ask user course info and store in variable
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+
+            // Ask user for page number and store in variable
+            Console.WriteLine("What page number are you on?");
+            string pageNum = Console.ReadLine();
+
+            // Ask user if any help needed and store in variable
+            Console.WriteLine("Do you need help with anything? Please answere true or false");
+            bool boolHelp = true;
+            string strHelp = Convert.ToString(boolHelp);
+
+            // Ask user about experience and store in variable
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string shareExp = Console.ReadLine();
+
+            // Ask user for feedback and store in variable
+            Console.WriteLine("Is there any feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            // Ask user for hours worked and store in variable
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+
+            // End program
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+        }
+    }
+}
