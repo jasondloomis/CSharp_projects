495a1279cdab063b01b5192d47f75ad62a82886e
Jason Loomis <jasondloomis@icloud.com>
2022-01-18 21:35:44 -0800

    add insurance application project

---
+++ ApplicationQualification/ApplicationQualification/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ApplicationQualification
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Intro splash
+            Console.WriteLine("Car Insurance Questionaire");
+            // Ask user for age
+            Console.WriteLine("Enter your age:");
+            string age = Console.ReadLine();
+            // Ask user for DUI convictions
+            Console.WriteLine("Have you ever been convicted of a DUI? Please enter \"true\" or \"false\"");
+            string dui = Console.ReadLine();
+            // Ask user for speeding ticket citations
+            Console.WriteLine("Have you ever had a speeding ticket? Please enter the number of how many.");
+            string tickets = Console.ReadLine();
+            // check all three conditions for true to be qualified
+            bool isQualified = (Convert.ToInt16(age) > 15 && Convert.ToBoolean(dui) == false && Convert.ToInt16(tickets) < 3);
+            Console.WriteLine(isQualified);
+        }
+    }
+}
