dd6f959a6fdc1492fe8c3041c5fa5745c8c63c89
Jason Loomis <jasondloomis@icloud.com>
2022-01-21 14:41:57 -0800

    add do while project

---
+++ ConsoleDoWhileBool/ConsoleDoWhileBool/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleDoWhileBool
+{
+    class Program
+    {
+        static void Main()
+        {
+            // declare variables
+            int n = 10;
+            bool i = n == 1;
+            // check first condition, n
+            while (n>=1)
+            {
+                // check second condiiton, i
+                do
+                {
+                    // display n on console
+                    Console.WriteLine(n);
+                    // subtract 1 from variable
+                    n--;
+                }
+                while (i);
+            }
+            // complete conditions and display
+            Console.WriteLine("Loops complete!");
+            Console.Read();
+        }
+    }
+}
