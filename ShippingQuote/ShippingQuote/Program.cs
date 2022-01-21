fd2efb4a9a3aa8bfa80fefb9266bc9fd6b626f73
Jason Loomis <jasondloomis@icloud.com>
2022-01-21 11:05:52 -0800

    add comments

--- ShippingQuote/ShippingQuote/Program.cs
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -11,7 +11,7 @@ namespace ShippingQuote
             // ask for weight of package and store in variable
             Console.WriteLine("Enter weight of package:");
             string weight = Console.ReadLine();
-
+            // enter width of package and check if weight is greater than 50lbs
             Console.WriteLine("Enter width of package:");
             string width = Console.ReadLine();
 
