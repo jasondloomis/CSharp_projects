4079d4da759d3bbc5f54c7373f9d54a615538758
Jason Loomis <jasondloomis@icloud.com>
2022-01-20 20:51:08 -0800

    start shipping quote project

---
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Display intro
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            // ask for weight of package and store in variable
+            Console.WriteLine("Enter weight of package:");
+            string weight = Console.ReadLine();
+
+            Console.WriteLine("Enter width of package:");
+            string width = Console.ReadLine();
+
+
+
+        }
+    }
+}
