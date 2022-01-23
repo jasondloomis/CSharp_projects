06a952c73ee4bdf9f59b7caa8d94cef4a43269e5
Jason Loomis <jasondloomis@icloud.com>
2022-01-22 18:05:10 -0800

    add first assignment

---
+++ ConsoleAssign/ConsoleAssign/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleAssign
+{
+    class Program
+    {
+        static void Main()
+        {
+            // declare string array and ask user for a noun
+            string[] strLibsArr = { "What", "came", "first", "the", "chicken", "or", "the", "noun", "Love", "is", "what", "makes", "noun", "go", "round"  };
+            Console.WriteLine("Enter a noun to add to the sentence:");
+            string strWord = Console.ReadLine();
+            // replace all words with user input
+            for (int i = 0; i < strLibsArr.Length; i++)
+            {
+                if (strLibsArr[i] == "noun")
+                {
+                    strLibsArr[i] = strWord;
+                }
+            }
+            // display array
+            foreach (string word in strLibsArr)
+            {
+                Console.WriteLine(word);
+            }
+            //Console.ReadLine();
+        }
+    }
+}
