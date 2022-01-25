292b86a4c3a7b5d224d4360ea1553f3e63f3b4fd
Jason Loomis <jasondloomis@icloud.com>
2022-01-24 19:54:51 -0800

    correct assisgnment 1

--- ConsoleAssign/ConsoleAssign/Program.cs
+++ ConsoleAssign/ConsoleAssign/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleAssign
         {
             // assignment 1
             // declare string array and ask user for a noun
-            string[] strLibsArr = { "What", "came", "first", "the", "chicken", "or", "the", "noun", "Love", "is", "what", "makes", "noun", "go", "round"  };
+            string[] strLibsArr = { "What", "came", "first", "the", "chicken", "or", "the", "noun", 
+                "Love", "is", "what", "makes", "noun", "go", "round"  };
             Console.WriteLine("Enter a noun to add to the sentence:"); 
             string strWord = Console.ReadLine();
             // replace all words with user input
@@ -24,7 +25,7 @@ namespace ConsoleAssign
             // display array
             foreach (string word in strLibsArr)
             {
-                Console.WriteLine(word);
+                Console.WriteLine(word + " ");
             }
 
             // assignment 2
