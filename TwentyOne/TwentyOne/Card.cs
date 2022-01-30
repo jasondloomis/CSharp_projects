acc0071281b83854f869d81a3ae3f5d1ea4a52f2
Jason Loomis <jasondloomis@icloud.com>
2022-01-30 11:22:36 -0800

    add deck of cards class

--- TwentyOne/TwentyOne/Card.cs
+++ TwentyOne/TwentyOne/Card.cs
@@ -9,6 +9,11 @@ namespace TwentyOne
     public class Card
     {
         
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
         public string Suit { get; set; }
         public string Face { get; set; }
 
