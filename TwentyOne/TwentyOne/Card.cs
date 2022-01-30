998769b7bca01e197d085003b27e33fc90500f75
Jason Loomis <jasondloomis@icloud.com>
2022-01-30 08:58:54 -0800

    add card class

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        
+        public string Suit { get; set; }
+        public string Face { get; set; }
+
+    }
+}
