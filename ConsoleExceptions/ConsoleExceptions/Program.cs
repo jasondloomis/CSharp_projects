60ab95b7ea5077d25901d1e860d436c4a04467c3
Jason Loomis <jasondloomis@icloud.com>
2022-01-30 08:42:13 -0800

    add exceptions assignment

---
+++ ConsoleExceptions/ConsoleExceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number:");
+                int numOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick another number:");
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the number...");
+                int numThree = numOne / numTwo;
+                Console.WriteLine(numOne + " divided by " + numTwo + " equals " + numThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please use a whole number:");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divede by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
